ea82ce6c4c5e97d5a16201f540da57443bc07b27
rbedemann <robin@vw-dilab.com>
2018-11-07 14:58:29 +0100

    show connections between suppliers and companies

--- src/app/overview/overview.component.ts
+++ src/app/overview/overview.component.ts
@@ -6,29 +6,45 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./overview.component.less']
 })
 export class OverviewComponent implements OnInit {
-  markers: LocationMarker[] = [];
+  companies: LocationMarker[] = [
+    {
+      label: 'Company 1',
+      lat: 52.5243700,
+      lng: 13.4105300,
+      suppliers: [
+        {
+          label: 'Supplier 1',
+          lat: 20.5243700,
+          lng: 18.4105300
+        },
+        {
+          label: 'Supplier 2',
+          lat: -50.541062,
+          lng: -44.616177
+        },
+        {
+          label: 'Supplier 3',
+          lat: 23.9075000,
+          lng: 120.3972300
+        }]
+    },
+    {
+      label: 'Company 2',
+      lat: -23.541062,
+      lng: -46.616177
+    },
+    {
+      label: 'Company 3',
+      lat: 39.9075000,
+      lng: 116.3972300
+    }];
+
+
 
   constructor() {
   }
 
   ngOnInit() {
-    this.markers.push(
-      {
-        label: 'Berlin',
-        lat: 52.5243700,
-        lng: 13.4105300
-      },
-      {
-        label: 'São Paulo',
-        lat: -23.541062,
-        lng: -46.616177
-      },
-      {
-        label: 'Beijing',
-        lat: 39.9075000,
-        lng: 116.3972300
-      }
-    )
   }
 
 }
