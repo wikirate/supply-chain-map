50843371523dd1b148f26be28aa81223b23a8c2e
RussellSnyder <russell@rustybass.com>
2018-11-07 12:14:15 +0100

    basic layout implemented and basic map integrated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,20 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
-
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {OverviewComponent} from "./overview/overview.component";
 import {AgmCoreModule} from "@agm/core";
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { CompanyComponent } from './company/company.component';
 
 @NgModule({
   declarations: [
     OverviewComponent,
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    CompanyComponent
   ],
   imports: [
     BrowserModule,
