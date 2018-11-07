3783733027414f6b7bcb6486755ba5101e9deb63
rbedemann <robin@vw-dilab.com>
2018-11-07 11:46:34 +0100

    add initial map component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {OverviewComponent} from "./overview/overview.component";
+import {AgmCoreModule} from "@agm/core";
 
 @NgModule({
   declarations: [
+    OverviewComponent,
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AgmCoreModule.forRoot({apiKey: 'AIzaSyB-TELjOeFgDQumKiPhhJeYXlF4mMGdKDU'})
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
