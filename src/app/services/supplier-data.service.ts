390946b7bee979f8ebf374ff2d16d94932a75928
Hackathon <hackathon@SDCs-MacBook-Air.local>
2018-11-07 12:13:36 +0100

    adds supplier-data service

---
+++ src/app/services/supplier-data.service.ts
@@ -0,0 +1,18 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Company} from '../types/company';
+import {Observable} from 'rxjs';
+import {map} from 'rxjs/operators';
+
+@Injectable()
+export class SupplierDataService {
+
+  private BASE_URL: string = 'https://wikirate.org';
+
+  constructor(private http: HttpClient) {}
+
+  getCompanyList() : Observable<Company[]> {
+    return this.http.get(`${this.BASE_URL}/Commons+Supplied_By+Record.json`)
+      .pipe(map((data: any) => data.items as Company[]));
+  }
+}
---
+++ src/app/types/company.ts
@@ -0,0 +1,7 @@
+export type Company = {
+  id: number,
+  name: string,
+  company: string,
+  value: number,
+  year: number
+}
